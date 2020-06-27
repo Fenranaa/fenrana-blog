c44db38bdab45ffeda0d2b86234561ed20256b58
fenrana <fenranaa@gmail.com>
2020-06-27 10:34:13 +0800

    文章显示功能完善

--- blog-vue/src/views/Article.vue
+++ blog-vue/src/views/Article.vue
@@ -2,12 +2,13 @@
   <div class="article">
     <Header />
     <div class="article-body">
-      <div class="article-body-left">
+      <div class="article-body-left" ref="body-left">
         <mavon-editor
           :defaultOpen="'preview'"
           :toolbarsFlag="false"
           :subfield="false"
           v-model="article.content"
+          :ishljs="true"
         />
       </div>
       <div class="article-body-right">
@@ -16,6 +17,7 @@
             v-for="(item, index) in catalog"
             :key="index"
             class="directory-item"
+            :class="index === directoryIndex ? 'active' : ''"
           >
             <a
               :class="generateDirectoryClassName(item.level)"
@@ -30,6 +32,7 @@
 </template>
 
 <script>
+var elementResizeDetectorMaker = require("element-resize-detector");
 import Header from "../components/Header";
 import { getRequest } from "../utils/request";
 import { httpCodeValidate } from "../utils/HttpCodeValidate";
@@ -39,7 +42,10 @@ export default {
   data() {
     return {
       article: {},
-      catalog: {}
+      catalog: {},
+      clientHeight: null, //屏幕高度
+      directoryIndex: 0,
+      hElement: []
     };
   },
   created() {
@@ -59,17 +65,55 @@ export default {
   },
   updated() {},
   methods: {
+    //添加定时器，组织scroll执行的次数过多
+    debounce(func, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this,
+          args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    },
+    isInViewPortOfTwo(el) {
+      const viewPortHeight =
+        window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight;
+      const top = el.getBoundingClientRect() && el.getBoundingClientRect().top;
+      window.console.log("top", top);
+      return top <= viewPortHeight;
+    },
+    //设置目录的央视
+    setDirectoryStyle(left, top, height) {
+      const articleBodyRightDom = document.querySelector(".article-body-right");
+      if (left) {
+        articleBodyRightDom.style.left = left + "px";
+      }
+      if (top) {
+        articleBodyRightDom.style.top = top + "px";
+      }
+      if (height) {
+        articleBodyRightDom.style.height = height + "px";
+      }
+    },
     generateDirectoryClassName(index) {
       return "directory-item-link-" + index;
     },
     generateDirectory() {
-      window.console.log("开始渲染目录");
       var articleBody = document.querySelector(".v-show-content");
-      window.console.log(articleBody);
+
       const nodes = ["H1", "H2", "H3", "H4"];
       let titles = [];
       articleBody.childNodes.forEach((e, index) => {
         if (nodes.includes(e.nodeName)) {
+          this.hElement.push(e);
           const id = "header-" + index;
           e.setAttribute("id", id);
           var title = e.innerHTML + "";
@@ -83,6 +127,63 @@ export default {
         }
       });
       this.catalog = titles;
+      /**
+       * 监听文章主体的宽度，当宽度变化时，修改文章的目录的left
+       * */
+      var erd = elementResizeDetectorMaker();
+      erd.listenTo(
+        document.getElementsByClassName("article-body-left"),
+        element => {
+          this.setDirectoryStyle(
+            element.offsetLeft + element.offsetWidth + 20,
+            null,
+            null
+          );
+        }
+      );
+      //获取屏幕的高度
+      if (this.clientHeight === null) {
+        this.clientHeight =
+          window.innerHeight ||
+          document.documentElement.clientHeight ||
+          document.body.clientHeight;
+      }
+      this.setDirectoryStyle(null, null, this.clientHeight - 100);
+
+      //监听屏幕滚动
+      this.listenScroll();
+    },
+    //监听屏幕滚动，随着屏幕的滚动，调节目录的位置，
+    listenScroll() {
+      // const articleBodyLeftDom = document.querySelector(".article-body-left");
+      let articleBodyRightDom = document.querySelector(".article-body-right");
+      let directoryDom = document.getElementsByClassName("directory-item");
+      window.addEventListener(
+        "scroll",
+        this.debounce(() => {
+          let scrollTop =
+            document.documentElement.scrollTop || document.body.scrollTop;
+          if (scrollTop > 60) {
+            this.setDirectoryStyle(null, 5, this.clientHeight - 25);
+          } else {
+            this.setDirectoryStyle(null, 80, this.clientHeight - 100);
+          }
+          window.console.log("滚动方法");
+          for (let i = 0; i < this.hElement.length; i++) {
+            let a = this.hElement[i];
+            let aTop = a.offsetTop;
+            if (scrollTop > aTop) {
+              this.directoryIndex = i;
+              if (directoryDom[i].offsetTop > this.clientHeight - 50) {
+                articleBodyRightDom.scrollTop = directoryDom[i].offsetTop - 100;
+              }
+            } else {
+              break;
+            }
+          }
+        }, 250),
+        true
+      );
     }
   },
   components: {
@@ -97,33 +198,37 @@ export default {
   .article-body {
     width: 70%;
     margin: 10px auto;
-    background-color: #fff;
-    /*display: flex;*/
-    /*justify-content: space-between;*/
 
     .article-body-left {
       float: left;
       width: 70%;
+      background-color: #ffffff;
     }
 
     .article-body-right {
-        background-color: #42b983;
+      overflow: auto;
       float: right;
-      width: 25%;
-      position: relative;
-      top: 0px;
-      /*right: 100px;*/
-        right: 0px;
+      width: 20%;
+      &::-webkit-scrollbar {
+        width: 4px;
+      }
+      &::-webkit-scrollbar-thumb {
+        /*background-color: #42b983;*/
+        background: rgba(0, 0, 0, 0.26);
+      }
+      position: fixed;
       .directory {
         padding: 0;
         margin: 0;
-
+        .active {
+          border-left: 2px solid red !important;
+        }
         .directory-item {
+          box-sizing: border-box;
           margin: 0;
           list-style: none;
           overflow: hidden;
-          border-left: 2px solid #e8e8e8;
-
+          border-left: 2px solid #ac9b9b;
           .directory-item-link {
             cursor: pointer;
             text-decoration: none;
